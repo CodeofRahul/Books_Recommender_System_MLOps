4e70a89c2d66a27c9da4092085f89b9456c334ed
CodeofRahul <420kumarahul@gmail.com>
2025-07-02 20:07:52 +0530

    logging file added

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from books_recommender.exception import AppException
 import sys
+from books_recommender.logger.log import logging
 
 try:
     a = 3/0
 except Exception as e:
+    logging.info(e)
     raise AppException(e, sys) from e
